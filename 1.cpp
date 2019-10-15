c603395dfbb29bddbb3064235b0735b43f403612
AluisioIfpr <54290557+AluisioIfpr@users.noreply.github.com>
2019-10-14 21:56:35 -0300

    Update 1.cpp
    Não esta funcionando mas compila certim

--- 1.cpp
+++ 1.cpp
@@ -1,76 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
-int main (){
+void Megasena (int NumeroAleatorio[15], int NumerosJogados){
+    for (int i = 0; i <= NumerosJogados; i++){                   //Linhas 8 e 9 servem para gerar um numero aleatorio
+        NumeroAleatorio[i] = rand()% 60 + 1;
+        repetir:
+        if (NumeroAleatorio[i] == NumeroAleatorio[i]){ // Linhas 11, 12 servem para checar se o numero Ã© repetido e gerar um novo
+            NumeroAleatorio[i] = rand()% 60 + 1;
+            goto repetir;  // Linha 13 volta para a linha 10 para checar denovo, caso gere outro numero repetido, sera trocado novamente e assim atÃ© que nao seja mais.
+        }
 
-vector<int> numeros;
-    setlocale(LC_ALL, "Portuguese");
-        int rep=0, soma = 0 , control= 0, i;
-
-
-        numeros.push_back(6);
-
-    cout << "Digite qualquer numero inteiro (exceto 0) para realizar uma operação ou digite [0] para sair" << endl;
-
-    for(int i=0; i>=control; i++){
-
-         cin >> numeros[i];
-
-         if( numeros[i] == 0 ){
-
-            break;
-
-         }
+    }
+    for (int i = 0; i <=NumerosJogados; i++){
+        cout << NumeroAleatorio[i] << " ";
     }
 
-    int decisao=1;
-    while (decisao != 0 ){
 
-        cout << endl << endl;
-        cout << "Digite [1] para: Imprimir a somatória de seus elementos\n";
-        cout << "Digite [2] para: Imprimir a média de seus elementos\n";
-        cout << "Digite [3] para: Imprimir a média e a somatória\n";
-        cout << "Digite [4] para: Substituir por zero todos os valores negativos e imprimir a média\n";
-        cout << "Digite [5] para: Substituir por zero todos os valores repetidos e imprimir a média e a somatória\n";
-        cout << "Digite [6] para: Mostrar vetor ordenado\n\n";
+    }
 
 
-    cin >> decisao;
-    cout << endl << endl;
 
 
-    switch(decisao){
+int main(){
+    int NumeroAleatorio[15], NumerosJogados;
+    setlocale(LC_ALL, "Portuguese");
+    cout << "Digite Quantos Numeros deseja jogar" << endl;
+    cin >> NumerosJogados;
+    Megasena(NumeroAleatorio, NumerosJogados);
 
-    case 1:
-        for(int Dyogao = 0; Dyogao < 5  ; Dyogao++)
 
-        {
-            soma += numeros[Dyogao];
-        }
-            cout << "A somatória de todos os elementos é: " << soma << endl;
-            break;
 
-    case 2:
-        for(int Dyogao = 0; Dyogao < 5  ; Dyogao++)
 
-        {
-            soma += numeros[Dyogao];
-        }
 
-        soma = soma / numeros[i];
-        cout << "";
-            break;
 
-    case 3:
-        cout << "teste\n";
-            break;
-    default:
-        cout << "Adios, volte sempre" << endl;
-        break;
 
- }}
-  return 0;
+    return 0;
 }
-
